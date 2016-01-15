f4546901217c1a0aafd06fbaa35988a80f560959
Yu-Ru Huang <mary@linshunghuang.com>
2016-01-15 17:38:17 +0800

    no change

--- np_loadtext.py
+++ np_loadtext.py
@@ -13,3 +13,13 @@ import numpy as np
 input_filename="input//distancepairs.csv"
 
 ii=np.loadtxt(input_filename, delimiter=',')
+
+
+def fetch_data_and_header(file_path_name):
+    table_values = np.loadtxt(file_path_name, delimiter=",", skiprows=1)
+
+    with open(file_path_name,'rU') as csvfile:
+        contents = csv.reader(csvfile)
+        header_list = np.asarray(next(contents))
+    
+    return table_values, header_list  
